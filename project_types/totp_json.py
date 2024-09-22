5e5e79b70c3e4b6afe001760cff0dec5a4a4ed0e
Jonathan Falcon <JQFalcon@outlook.com>
2024-09-21 21:15:28 -0500

    Modified `totp_json.py` to make `documenation` property optional

--- project_types/totp_json.py
+++ project_types/totp_json.py
@@ -2,7 +2,7 @@ from typing import Dict, List, Optional, TypedDict
 
 class TotpJsonValues(TypedDict):
     methods: List[str]
-    documentation: str
+    documentation: Optional[str]
     recovery: Optional[str]
 
 Type_TotpJson = Dict[str, TotpJsonValues]
